29c22252c2a23a2030356fb292bee944ba6a3261
wahyudwiananda <wahyuananda39@gmail.com>
2022-12-16 00:26:51 +0700

    add background

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function predictWebcam() {
       if (predictions[n].score > 0.66) {
         const p = document.createElement("p");
         p.innerText = predictions[n].class + " - with " + Math.round(parseFloat(predictions[n].score) * 100) + "% confidence.";
-        p.style = "margin-left: " + predictions[n].bbox[0] + "px; margin-top: " + (predictions[n].bbox[1] - 10) + "px; width: " + (predictions[n].bbox[2] - 10) + "px; top: 0; left: 0;";
+        p.style = "margin-left: " + predictions[n].bbox[0] + "px; margin-top: " + predictions[n].bbox[1] + "px; width: " + predictions[n].bbox[2] + "px; top: 0; left: 0;";
 
         const highlighter = document.createElement("div");
         highlighter.setAttribute("class", "highlighter");
